a3b0849e3e60d19be4990aae11f8472d1655e6f8
Laurel Younis <laurelyounisdev@gmail.com>
2022-09-17 12:14:03 -0700

    added dashboard home & login pages, added auth modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,30 @@
 import whiteLogo from '../images/blacklogo.png'
 import colorLogo from '../images/redlogo.png'
 
-const Nav= ({minimal, authToken}) => {
+const Nav= ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
 
+    const handleClick= ()=> {
+        setShowModal(true)
+        //when click on the button changes state of setIsSignUp to false
+        setIsSignUp(false)
+    }
 
     return(
         <nav>
         <div className="logo-container">
-            <img className="logo" src={minimal ? colorLogo : whiteLogo} />
+            <img 
+                className="logo" 
+                src={minimal ? colorLogo : whiteLogo}
+                alt="Coffee Chat Logo" />
         </div>
-        {!authToken && !minimal && <button 
-            className="nav-button">Log in</button>}
+        {!authToken && !minimal && (
+        <button 
+            className="nav-button"
+            onClick={handleClick}
+            disabled={showModal}
+            >
+                Log in
+                </button>)}
     </nav>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,26 +4,39 @@ import {useState} from 'react'
 
 const Home= () =>{
     const[showModal, setShowModal]= useState(false)
+    //where the state for is sign up is stored
+    const[isSignUp, setIsSignUp]= useState(true)
+
     const authToken= false
+
     const handleClick= () => {
         console.log('clicked')
         setShowModal(true)
+        setIsSignUp(true)
     }
 
     return(
-        <div className='overlay'>
-        <Nav minimal={false} authToken={authToken}/>
+        <div className="overlay">
+        <Nav 
+        minimal={false} 
+        authToken={authToken} 
+        setShowModal={setShowModal} 
+        showModal={showModal}
+        //passing through the state for is sign up / option to change it (logic is in the Nav component: so when we click on the button (const handleclick) it changes setIsSignUp to false
+        setIsSignUp={setIsSignUp}
+        />
+
         <div className="home">
-            <h1>Get Linked ®</h1>
-            <button className={"primary-button"} onClick={handleClick}>
-                {authToken ? 'Signout' : "Create Account"}
+            <h1 className='primary-title'>Get Linked  ®</h1>
+            <button className="primary-button" onClick={handleClick}>
+                {authToken ? "Signout" : "Create Account"}
             </button>
 
             {showModal && (
-                <AuthModal setShowModal={setShowModal}/>
+                <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
             )}
 
-            
+
         </div>
         </div>
     )
